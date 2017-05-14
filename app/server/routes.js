a4cf9d6342110b08af59034fd5f7ef8787985f7b
Sveta1528 <degtyareva1506@mail.ru>
2017-05-14 14:05:59 +0300

    Added method get

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,9 +1,16 @@
-
 module.exports = function(app, db) {
   // POST
   app.post('/records', (req, res) => {
       db.collection('records').insert({ text: req.body.body, title: req.body.title }, (err, result) => {
         if (!err) { res.send('Record added');}
+        else { res.send('Error')}
+      });
+    });
+
+    // GET
+    app.get('/records/:title', (req, res) => {
+      db.collection('records').findOne({ 'title': req.params.title }, (err, result) => {
+        if (!err) { res.send(result);}
         else { res.send('Error')}
       });
     });
